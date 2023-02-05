357fca867759ec56b25bf63b1e79a03da7772dbc
dzp-datah <damita.f@ku.th>
2023-02-05 14:11:51 +0700

    add bright

--- main.cpp
+++ main.cpp
@@ -1,19 +1,26 @@
 #include <Arduino.h>
-#include <Bounce2.h>
-#define BUTTON 27
-int cnt = 0;
-Bounce debouncer = Bounce();
-void setup() {
-    Serial.begin(115200);
-    Serial.println("BUTTON");
-    debouncer.attach(BUTTON, INPUT_PULLUP);
-    debouncer.interval(25); 
-}
+#define GREEN 33
+#define YELLOW 25
+#define RED 26
 
-void loop() {
-    debouncer.update();
-    if ( debouncer.fell() ) { 
-        cnt++;
-        Serial.println(cnt);
-    }
+void setup()
+{
+    ledcSetup(0, 5000, 8);
+    ledcAttachPin(GREEN, 0);
+    ledcSetup(1, 5000, 8);
+    ledcAttachPin(YELLOW, 1);
+    ledcSetup(2, 5000, 8);
+    ledcAttachPin(RED, 2);
+}
+void loop()
+{
+  for (int i = 0; i < 255; i++) {
+    ledcWrite(0, i);
+    delay(5);
+  }
+  for (int i = 255; i > 0; i--) {
+    ledcWrite(0, i);
+    delay(5);
+  }
+  delay(1000);
 }
