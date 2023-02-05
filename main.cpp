d079dd0c0da931c465f429076ed88baf4d7b6983
dzp-datah <damita.f@ku.th>
2023-02-05 13:27:51 +0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include <Arduino.h>
+void setup() {
+    Serial.begin(115200);
+}
+
+void loop() {
+    Serial.println("Hello World");
+    delay(1000);
+}
+
+// switch
+#include <Arduino.h>
+#define BUTTON 27
+int cnt = 0;
+void setup() {
+    Serial.begin(115200);
+    Serial.println("");
+    pinMode(BUTTON, INPUT_PULLUP);
+}
+
+void loop() {
+    if (!digitalRead(BUTTON)){
+        cnt++;
+        Serial.println(cnt);
+    }
+}
+
+// 5 sec
+#include <Arduino.h>
+#define GREEN 33
+void setup() {
+    pinMode(GREEN, OUTPUT);
+    digitalWrite(GREEN,0);
+}
+
+void loop() {
+    digitalWrite(GREEN,1);
+    delay(500);
+    digitalWrite(GREEN,0);
+    delay(500);
+}
